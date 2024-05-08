f94a38cdf1766c368f7d4ae49028d77a114121c1
Gabriel Binotti <gabrielbinotti.dev@gmail.com>
2024-05-08 16:36:41 -0300

    Projeto em Flask

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', title="Home Page", description="Welcome to my Flask app!")
+    return render_template('index.html', title="Home")
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About")
 
 if __name__ == '__main__':
     app.run(debug=True)
